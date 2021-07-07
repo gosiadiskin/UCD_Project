d4fe1cc41475ea63743a4991e7cf183812fc7771
gosiadiskin <gosiadiskin@gmail.com>
2021-07-07 17:51:20 +0100

    Updated with 'Shape' command

--- Merging.py
+++ Merging.py
@@ -2,9 +2,11 @@ import pandas as pd
 pd.set_option('display.expand_frame_repr', False)
 Data=pd.read_csv('indexData.csv')
 print(Data)
+print(Data.shape)
 
 Info=pd.read_csv('indexInfo.csv')
 print(Info)
+print(Info.shape)
 
 Data_Info=Data.merge(Info, on='Index', suffixes=('_Dat', '_Inf'))
 print(Data_Info)
